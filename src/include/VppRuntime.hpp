511f1523dbc67b72c928dd74d8e90a7174af37db
Neale Ranns <nranns@cisco.com>
2018-12-14 01:35:12 -0800

    Runtime
    Signed-off-by: Neale Ranns <nranns@cisco.com>

---
+++ src/include/VppRuntime.hpp
@@ -0,0 +1,51 @@
+/* -*- C++ -*-; c-basic-offset: 4; indent-tabs-mode: nil */
+/*
+ * Copyright (c) 2018 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+#ifndef __VPP_RUNTIME_H__
+#define __VPP_RUNTIME_H__
+
+#include <opflexagent/Agent.h>
+
+#include "VppIdGen.hpp"
+#include "VppUplink.hpp"
+#include "VppVirtualRouter.hpp"
+
+namespace VPP
+{
+struct Runtime
+{
+    Runtime(opflexagent::Agent &agent_, opflexagent::IdGenerator &idGen)
+        : agent(agent_)
+        , id_gen(idGen)
+        , uplink(agent)
+    {
+    }
+    /**
+     * Referene to the uber-agent
+     */
+    opflexagent::Agent &agent;
+    /**
+     * ID generator instance
+     */
+    IdGen id_gen;
+    /**
+     * Uplink interface manager
+     */
+    Uplink uplink;
+    /**
+     * Virtual Router Settings
+     */
+    std::shared_ptr<VirtualRouter> vr;
+
+  private:
+    Runtime(const Runtime &);
+};
+}; // namespace VPP
+
+#endif
